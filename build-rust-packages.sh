7ffc5385ddcb6af352e8017e4c6ff142d3033131
Joey Cruz <cruzjoey94@gmail.com>
2019-05-12 19:06:59 -0400

    Travis ci integration (#2)

--- build-rust-packages.sh
+++ build-rust-packages.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Building wasm files"
 # Build the wasm file(s)
 pushd rust
 wasm-pack build
 popd
 
+echo "Creating yarn link for game-pop-data-transforms"
 # Link the js files using yarn
 pushd rust/pkg
 yarn link
 popd
 
+echo "Linking game-pop-data-transforms"
 # Finally link it for js files
 yarn link game-pop-data-transforms
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+describe('ph test', () => {
+	it('passes', () => {
+		expect(1).toEqual(1);
+	});
+});
